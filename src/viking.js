beac39c0f3b5c28a7905d768e72b78eddaefe38c
hkmichae <heathermichaels@icloud.com>
2021-01-26 16:03:50 -0500

    Iteration 1: Soldier

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack () {
+        return this.strength
+    }
+    receiveDamage (damage) {
+        this.health -= damage
+    }
+}
+let rambo = new Soldier(500,10)
+rambo.receiveDamage(99)
 
 // Viking
 class Viking {}
